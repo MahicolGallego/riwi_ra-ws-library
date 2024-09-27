95f029805e1160ea12bef11958aa34603fd43c87
Mahi_G13 <maicolgallego14@gmail.com>
2024-09-27 16:26:01 -0500

    chore(database): configure typeorm connection and models

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,1 +1,41 @@
-export class Permission {}
+import { Roles } from 'src/common/constants/role.enum';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Permission {
+  @PrimaryColumn({ nullable: false })
+  @Column({ type: 'varchar', length: 20, enum: Roles })
+  entity: string;
+
+  @PrimaryColumn({ nullable: false })
+  @Column({ type: 'enum', enum: Roles })
+  role: string;
+
+  @Column({ type: 'boolean' })
+  write: boolean;
+
+  @Column({ type: 'boolean' })
+  read: boolean;
+
+  @Column({ type: 'boolean' })
+  update: boolean;
+
+  @Column({ type: 'boolean' })
+  delete: boolean;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
