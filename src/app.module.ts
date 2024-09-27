4d3ebf0d578be8fe83dd038f77545f4d93f892b8
Mahi_G13 <maicolgallego14@gmail.com>
2024-09-26 21:41:56 -0500

    wip: define database models and set up TypeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configservice: ConfigService) => ({}),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
